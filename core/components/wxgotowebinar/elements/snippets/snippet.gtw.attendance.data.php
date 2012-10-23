610e7032bd38cb9de23921b95ad25182f635e8d2
Eli Snyder <freejung@gmail.com>
2012-10-23 00:19:21 -0600

    minor changes to attendance data snippet

--- core/components/wxgotowebinar/elements/snippets/snippet.gtw.attendance.data.php
+++ core/components/wxgotowebinar/elements/snippets/snippet.gtw.attendance.data.php
@@ -34,77 +34,81 @@ $questionTpl = $modx->getOption('questionTpl',$scriptProperties,'');
 $answerTpl = $modx->getOption('answerTpl',$scriptProperties,'');
 $getSurveys = $modx->getOption('getSurveys',$scriptProperties,0);
 $surveyTpl = $modx->getOption('surveyTpl',$scriptProperties,'');
+$default = $modx->getOption('default',$scriptProperties,'');
+
 
 $output = '';
 $registered = 0;
 if($prospectId) {
-	$gtw = $modx->getService('wxgotowebinar','wxGoToWebinar',$modx->getOption('wxgotowebinar.core_path',null,$modx->getOption('core_path').'components/wxgotowebinar/').'model/wxgotowebinar/');
-	if (!($gtw instanceof wxGoToWebinar)) return 'could not instantiate wxGoToWebinar';
-	
-	$webinex = $modx->getService('webinex','Webinex',$modx->getOption('webinex.core_path',null,$modx->getOption('core_path').'components/webinex/').'model/webinex/',$scriptProperties);
-	if (!($webinex instanceof Webinex)) return 'could not instantiate Webinex';
-	if($prospect = $modx->getObject('wxProspect',$prospectId)){
-	    if($registrations = $prospect->getMany('Registration',array('presentation' => $presentationId))){
-	    	$registered = 1;
-	        foreach ($registrations as $registration) {
-	            if($registrants = $modx->getCollection('wxGtwRegistrant', array('wxregistration' => $registration->id))) {
-	            	$registrantText = '';
-	                foreach($registrants as $registrant) {
-	                    $questionsText = '';
-	                    if($getQuestions) {
-	                        if($questions = $registrant->getMany('Question')) {
-	                            foreach($questions as $question) {
-	                                $answerText = '';
-	                                if($answers = $question->getMany('Answer')){
-	                                    foreach ($answers as $answer) {
-	                                        $answerArray = $answer->toArray();
-	                                        if($thisAnswer = $modx->getChunk($answerTpl, $answerArray)){
-		                                        $answerText .= $thisAnswer;
-		                                    }
-	                                    }
-	                                }
-	                                $questionArray = $question->toArray();
-	                                $questionArray['answers'] = $answerText;
-	                                if($thisQuestion = $modx->getChunk($questionTpl, $questionArray)){
-		                                $questionsText .= $thisQuestion;
-		                            }
-	                            }
-	                        }
-	                    }
-	                    $surveysText = '';
-	                    if($getSurveys) {
-	                        if($pollAnswers = $registrant->getMany('PollAnswer')) {
-	                            foreach($pollAnswers as $pollAnswer) {
-	                                $response = $pollAnswer->getOne('Response');
-	                                $poll = $response->getOne('Poll');
-	                                $pollArray = $poll->toArray();
-	                                $pollArray['response'] = $response->get('text');
-	                                if($thisSurvey = $modx->getChunk($surveyTpl, $pollArray)){
-		                                $surveysText .= $thisSurvey;
-		                            }
-	                            }
-	                        }
-	                    }
-	                    $registrantArray = $registrant->toArray();
-	                    $registrantArray['questions'] = $questionsText;
-	                    $registrantArray['surveys'] = $surveysText;
-	                    $registrantArray['attendanceTimeInMinutes'] = round($registrantArray['attendanceTimeInSeconds']/60);
-	                    $hours = floor($registrantArray['attendanceTimeInSeconds'] / (60 * 60));
-	                    $divisor_for_minutes = $registrantArray['attendanceTimeInSeconds'] % (60 * 60);
-	                    $minutes = floor($divisor_for_minutes / 60);
-	                    $divisor_for_seconds = $divisor_for_minutes % 60;
-	                    $seconds = ceil($divisor_for_seconds);
-	                    $registrantArray['attendanceHours'] = $hours;
-	                    $registrantArray['attendanceMinutes'] = $minutes;
-	                    $registrantArray['attendanceSeconds'] = $seconds;
-	                    if($thisRegistrant = $modx->getChunk($registrantTpl, $registrantArray)){
-		                    $registrantText .= $thisRegistrant;
-		                }
-	                }
-	            }
-	        }
-	    }
-	    $output = $modx->getChunk($tpl, array('registered' => $registered, 'registrantText' => $registrantText));
-	}
+    $gtw = $modx->getService('wxgotowebinar','wxGoToWebinar',$modx->getOption('wxgotowebinar.core_path',null,$modx->getOption('core_path').'components/wxgotowebinar/').'model/wxgotowebinar/');
+    if (!($gtw instanceof wxGoToWebinar)) return 'could not instantiate wxGoToWebinar';
+    
+    $webinex = $modx->getService('webinex','Webinex',$modx->getOption('webinex.core_path',null,$modx->getOption('core_path').'components/webinex/').'model/webinex/',$scriptProperties);
+    if (!($webinex instanceof Webinex)) return 'could not instantiate Webinex';
+    if($prospect = $modx->getObject('wxProspect',$prospectId)){
+        if($registrations = $prospect->getMany('Registration',array('presentation' => $presentationId))){
+            $registered = 1;
+            foreach ($registrations as $registration) {
+                if($registrants = $modx->getCollection('wxGtwRegistrant', array('wxregistration' => $registration->id))) {
+                    $registrantText = '';
+                    foreach($registrants as $registrant) {
+                        $questionsText = '';
+                        if($getQuestions) {
+                            if($questions = $registrant->getMany('Question')) {
+                                foreach($questions as $question) {
+                                    $answerText = '';
+                                    if($answers = $question->getMany('Answer')){
+                                        foreach ($answers as $answer) {
+                                            $answerArray = $answer->toArray();
+                                            if($thisAnswer = $modx->getChunk($answerTpl, $answerArray)){
+                                                $answerText .= $thisAnswer;
+                                            }
+                                        }
+                                    }
+                                    $questionArray = $question->toArray();
+                                    $questionArray['answers'] = $answerText;
+                                    if($thisQuestion = $modx->getChunk($questionTpl, $questionArray)){
+                                        $questionsText .= $thisQuestion;
+                                    }
+                                }
+                            }
+                        }
+                        $surveysText = '';
+                        if($getSurveys) {
+                            if($pollAnswers = $registrant->getMany('PollAnswer')) {
+                                foreach($pollAnswers as $pollAnswer) {
+                                    $response = $pollAnswer->getOne('Response');
+                                    $poll = $response->getOne('Poll');
+                                    $pollArray = $poll->toArray();
+                                    $pollArray['response'] = $response->get('text');
+                                    if($thisSurvey = $modx->getChunk($surveyTpl, $pollArray)){
+                                        $surveysText .= $thisSurvey;
+                                    }
+                                }
+                            }
+                        }
+                        $registrantArray = $registrant->toArray();
+                        $registrantArray['questions'] = $questionsText;
+                        $registrantArray['surveys'] = $surveysText;
+                        $registrantArray['attendanceTimeInMinutes'] = round($registrantArray['attendanceTimeInSeconds']/60);
+                        $hours = floor($registrantArray['attendanceTimeInSeconds'] / (60 * 60));
+                        $divisor_for_minutes = $registrantArray['attendanceTimeInSeconds'] % (60 * 60);
+                        $minutes = floor($divisor_for_minutes / 60);
+                        $divisor_for_seconds = $divisor_for_minutes % 60;
+                        $seconds = ceil($divisor_for_seconds);
+                        $registrantArray['attendanceHours'] = $hours;
+                        $registrantArray['attendanceMinutes'] = $minutes;
+                        $registrantArray['attendanceSeconds'] = $seconds;
+                        if($thisRegistrant = $modx->getChunk($registrantTpl, $registrantArray)){
+                            $registrantText .= $thisRegistrant;
+                        }
+                    }
+                }
+            }
+        }
+        $output = $modx->getChunk($tpl, array('registered' => $registered, 'registrantText' => $registrantText, 'presentationId' => $presentationId, 'default' => $default));
+    }
+}else{
+    $output = $default;
 }
 return $output;
